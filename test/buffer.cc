d556960dc0665895bc54c5411ba710778e376139
codinghuang <15070967610@163.com>
2019-11-05 10:23:31 +0800

    test: add buffer test

--- test/buffer.cc
+++ test/buffer.cc
@@ -5,6 +5,95 @@
 using namespace std;
 using fsw::Buffer;
 
+TEST(buffer, construct)
+{
+    Buffer buffer(16);
+    ASSERT_EQ(buffer.size(), 16);
+    ASSERT_EQ(buffer.length(), 0);
+    ASSERT_NE(buffer.c_buffer(), nullptr);
+}
+
+TEST(buffer, length)
+{
+    char src_buffer[] = "fsw";
+    Buffer buffer(16);
+    buffer.append(src_buffer, sizeof(src_buffer) - 1);
+    ASSERT_EQ(buffer.length(), 3);
+}
+
+TEST(buffer, size)
+{
+    char src_buffer[] = "fsw";
+    Buffer buffer(16);
+    buffer.append(src_buffer, sizeof(src_buffer) - 1);
+    ASSERT_EQ(buffer.size(), 16);
+}
+
+TEST(buffer, c_buffer)
+{
+    char src_buffer[] = "fsw";
+    Buffer buffer(16);
+    buffer.append(src_buffer, sizeof(src_buffer) - 1);
+    ASSERT_EQ(strcmp(buffer.c_buffer(), "fsw"), 0);
+}
+
+TEST(buffer, clear)
+{
+    char src_buffer[] = "fsw";
+    Buffer buffer(16);
+    buffer.append(src_buffer, sizeof(src_buffer) - 1);
+    buffer.clear();
+    // Determines whether the length is 0
+    ASSERT_EQ(buffer.length(), 0);
+    // Determines whether the c_buffer is empty
+    ASSERT_EQ(strcmp(buffer.c_buffer(), ""), 0);
+}
+
+TEST(buffer, append_c_string)
+{
+    char src_buffer[] = "fsw";
+    Buffer buffer(16);
+    buffer.append(src_buffer, sizeof(src_buffer) - 1);
+    ASSERT_EQ(strcmp(buffer.c_buffer(), src_buffer), 0);
+    ASSERT_EQ(buffer.length(), sizeof(src_buffer) - 1);
+}
+
+TEST(buffer, append_cpp_string)
+{
+    std::string str = "fsw";
+    Buffer buffer(16);
+    buffer.append(str);
+    ASSERT_EQ(strcmp(buffer.c_buffer(), str.c_str()), 0);
+    ASSERT_EQ(buffer.length(), str.length());
+}
+
+TEST(buffer, append_int)
+{
+    char str1[] = "1";
+    char str2[] = "12";
+    Buffer buffer(16);
+
+    buffer.append(1);
+    ASSERT_EQ(buffer.length(), 1);
+    ASSERT_EQ(strcmp(buffer.c_buffer(), str1), 0);
+
+    buffer.append(2);
+    ASSERT_EQ(buffer.length(), 2);
+    ASSERT_EQ(strcmp(buffer.c_buffer(), str2), 0);
+}
+
+TEST(buffer, append_buffer)
+{
+    std::string str = "fsw";
+    Buffer buffer1(16);
+    Buffer buffer2(16);
+
+    buffer1.append(str);
+    buffer2.append(&buffer1);
+    ASSERT_EQ(strcmp(buffer2.c_buffer(), "fswfsw"), 0);
+    ASSERT_EQ(buffer2.length(), 2 * str.length());
+}
+
 TEST(buffer, append)
 {
     char src_buffer[] = "aa";
@@ -49,21 +138,6 @@ TEST(buffer, append)
     buffer5->append(src_str, strlen(src_str));
     ASSERT_EQ(buffer5->length(), 3);
     ASSERT_EQ(strcmp(buffer5->c_buffer(), "abc"), 0);
-}
-
-TEST(buffer, append_int)
-{
-    char str1[] = "1";
-    char str2[] = "12";
-    Buffer buffer(16);
-
-    buffer.append(1);
-    ASSERT_EQ(buffer.length(), 1);
-    ASSERT_EQ(strcmp(buffer.c_buffer(), str1), 0);
-
-    buffer.append(2);
-    ASSERT_EQ(buffer.length(), 2);
-    ASSERT_EQ(strcmp(buffer.c_buffer(), str2), 0);
 }
 
 TEST(buffer, dup)
