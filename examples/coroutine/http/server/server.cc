8ff9c3fbd353cc430ffbefcb89952f56e466974f
codinghuang <15070967610@163.com>
2019-11-06 11:08:20 +0800

    update http server example

--- examples/coroutine/http/server/server.cc
+++ examples/coroutine/http/server/server.cc
@@ -31,7 +31,7 @@ int main(int argc, char const *argv[])
         char ip[] = "127.0.0.1";
 
         Server *serv = new Server(ip, 80);
-        serv->set_handler(string("/index"), handler);
+        serv->set_handler("/index", handler);
         serv->start();
     });
 
