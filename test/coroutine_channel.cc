ab1fc58dd5676da8d75cfebc3e25530f0caf2524
codinghuang <15070967610@163.com>
2019-12-09 01:23:54 +0000

    fix: test

--- test/coroutine_channel.cc
+++ test/coroutine_channel.cc
@@ -7,26 +7,27 @@ using namespace fsw;
 using namespace std;
 using fsw::coroutine::Channel;
 
+static void pop(Channel *chan)
+{
+    void *data;
+    data = chan->pop();
+    ASSERT_EQ(*(string *)data, "hello world");
+}
+
+static void push(Channel *chan)
+{
+    bool ret;
+    string data = "hello world";
+    ret = chan->push(&data);
+    ASSERT_TRUE(ret);
+}
+
 TEST(coroutine_channel, pop_push)
 {
     Channel *chan = new Channel();
 
-    Coroutine::create([](void *arg)
-    {
-        void *data;
-        Channel *chan = (Channel *)arg;
-        data = chan->pop();
-        ASSERT_EQ(*(string *)data, "hello world");
-    }, (void *)chan);
-
-    Coroutine::create([](void *arg)
-    {
-        bool ret;
-        string data = "hello world";
-        Channel *chan = (Channel *)arg;
-        ret = chan->push(&data);
-        ASSERT_TRUE(ret);
-    }, (void *)chan);
+    Coroutine::create(std::bind(pop, chan));
+    Coroutine::create(std::bind(push, chan));
 
     delete chan;
 }
