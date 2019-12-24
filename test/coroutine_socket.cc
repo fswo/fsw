1eb71642b585424ef92c8117a4737b608ffc5b09
codinghuang <15070967610@163.com>
2019-12-24 01:51:39 +0000

    test: add coroutine::Socket test

---
+++ test/coroutine_socket.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "fsw/socket.h"
+#include "fsw/coroutine_socket.h"
+#include "gtest/gtest.h"
+
+using fsw::coroutine::Socket;
+
+TEST(coroutine_socket, Socket_domain_type_protocol)
+{
+    Socket *sock = new Socket(AF_INET, SOCK_STREAM, 0);
+    ASSERT_GT(sock->get_fd(), 2);
+}
+
+TEST(coroutine_socket, Socket_fd)
+{
+    int fd = open("/dev/null", O_RDONLY);
+    Socket *sock = new Socket(fd);
+    ASSERT_EQ(sock->get_fd(), fd);
+}
+
+TEST(coroutine_socket, bind)
+{
+    int ret;
+    char ip[] = "127.0.0.1";
+    Socket *sock = new Socket(AF_INET, SOCK_STREAM, 0);
+    ret = sock->bind(FSW_SOCK_TCP, ip, 80);
+    ASSERT_EQ(ret, 0);
+}
+
+TEST(coroutine_socket, listen)
+{
+    int ret;
+    char ip[] = "127.0.0.1";
+    Socket *sock = new Socket(AF_INET, SOCK_STREAM, 0);
+    sock->bind(FSW_SOCK_TCP, ip, 80);
+    ret = sock->listen(512);
+    ASSERT_EQ(ret, 0);
+}
+
