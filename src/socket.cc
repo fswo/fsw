3fadef53e83a2e1d6aa3c23cf1718e49c9a21e71
codinghuang <2812240764@qq.com>
2020-01-03 02:58:43 +0000

    feat: refactor the fswSocket

--- src/socket.cc
+++ src/socket.cc
@@ -16,7 +16,6 @@ int fswSocket_create(int domain, int type, int protocol)
 
 int fswSocket_bind(int sock, int type, char *host, int port)
 {
-    int ret;
     struct sockaddr_in servaddr;
 
     if (type == FSW_SOCK_TCP)
@@ -24,23 +23,22 @@ int fswSocket_bind(int sock, int type, char *host, int port)
         bzero(&servaddr, sizeof(servaddr));
         if (inet_aton(host, &(servaddr.sin_addr)) < 0)
         {
-            fswWarn("Error has occurred: (errno %d) %s", errno, strerror(errno));
+            return -1;
         }
         servaddr.sin_family = AF_INET;
         servaddr.sin_port = htons(port);
-        ret = bind(sock, (struct sockaddr *)&servaddr, sizeof(servaddr));
-        if (ret < 0)
+        if (bind(sock, (sockaddr *)&servaddr, sizeof(servaddr)) < 0)
         {
-            fswWarn("Error has occurred: (errno %d) %s", errno, strerror(errno));
-            return -1; 
+            return -1;
         }
     }
     else
     {
+        fswWarn("Error has occurred: %s", "the bind type is not supported");
         return -1;
     }
 
-    return ret;
+    return 0;
 }
 
 int fswSocket_accept(int sock)
@@ -51,11 +49,6 @@ int fswSocket_accept(int sock)
 
     len = sizeof(sa);
     connfd = accept(sock, (struct sockaddr *)&sa, &len);
-    if (connfd < 0 && errno != EAGAIN)
-    {
-        fswWarn("Error has occurred: (errno %d) %s", errno, strerror(errno));
-    }
-
     return connfd;
 }
 
@@ -64,10 +57,6 @@ int fswSocket_close(int fd)
     int ret;
 
     ret = close(fd);
-    if (ret < 0)
-    {
-        fswWarn("Error has occurred: (errno %d) %s", errno, strerror(errno));
-    }
     return ret;
 }
 
@@ -76,11 +65,6 @@ int fswSocket_shutdown(int sock, int how)
     int ret;
 
     ret = shutdown(sock, how);
-
-    if (ret < 0)
-    {
-        fswWarn("Error has occurred: (errno %d) %s", errno, strerror(errno));
-    }
     return ret;
 }
 
@@ -89,10 +73,6 @@ int fswSocket_set_option(int fd, int level, int optname, const void *optval, soc
     int ret;
 
     ret = setsockopt(fd, level, optname, optval, optlen);
-    if (ret < 0)
-    {
-        fswWarn("Error has occurred: (errno %d) %s", errno, strerror(errno));
-    }
     return ret;
 }
 
@@ -101,10 +81,6 @@ int fswSocket_get_option(int fd, int level, int optname, void *optval, socklen_t
     int ret;
 
     ret = getsockopt(fd, level, optname, optval, optlen);
-    if (ret < 0)
-    {
-        fswWarn("Error has occurred: (errno %d) %s", errno, strerror(errno));
-    }
     return ret;
 }
 
@@ -113,10 +89,6 @@ int fswSocket_getname(int fd, sockaddr *addr, socklen_t *len)
     int ret;
 
     ret = getsockname(fd, addr, len);
-    if (ret < 0)
-    {
-        fswWarn("Error has occurred: (errno %d) %s", errno, strerror(errno));
-    }
     return ret;
 }
 
@@ -125,10 +97,6 @@ int fswSocket_getpeername(int fd, sockaddr *addr, socklen_t *len)
     int ret;
 
     ret = getpeername(fd, addr, len);
-    if (ret < 0)
-    {
-        fswWarn("Error has occurred: (errno %d) %s", errno, strerror(errno));
-    }
     return ret;
 }
 
@@ -137,10 +105,6 @@ int fswSocket_listen(int sock, int backlog)
     int ret;
 
     ret = listen(sock, backlog);
-    if (ret < 0)
-    {
-        fswWarn("Error has occurred: (errno %d) %s", errno, strerror(errno));
-    }
     return ret;
 }
 
@@ -149,10 +113,6 @@ ssize_t fswSocket_recv(int sock, void *buf, size_t len, int flag)
     ssize_t ret;
 
     ret = recv(sock, buf, len, flag);
-    if (ret < 0 && errno != EAGAIN)
-    {
-        fswWarn("Error has occurred: (errno %d) %s", errno, strerror(errno));
-    }
     return ret;
 }
 
@@ -161,10 +121,6 @@ ssize_t fswSocket_send(int sock, const void *buf, size_t len, int flag)
     ssize_t ret;
 
     ret = send(sock, buf, len, flag);
-    if (ret < 0 && errno != EAGAIN)
-    {
-        fswWarn("Error has occurred: (errno %d) %s", errno, strerror(errno));
-    }
     return ret;
 }
 
@@ -175,14 +131,8 @@ int fswSocket_set_nonblock(int sock)
     flags = fcntl(sock, F_GETFL, 0);
     if (flags < 0)
     {
-        fswWarn("Error has occurred: (errno %d) %s", errno, strerror(errno));
         return -1;
     }
     flags = fcntl(sock, F_SETFL, flags | O_NONBLOCK);
-    if (flags < 0)
-    {
-        fswWarn("Error has occurred: (errno %d) %s", errno, strerror(errno));
-        return -1;
-    }
-    return 0;
+    return flags;
 }
