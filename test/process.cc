7de5e9c781296359cb68116de25ffbf5bd7953f5
codinghuang <15070967610@163.com>
2020-01-03 09:20:02 +0000

    test: TEST(process, construct_enable_coroutine)

--- test/process.cc
+++ test/process.cc
@@ -1,17 +1,30 @@
 #include "fsw/process.h"
 #include "fsw/fsw.h"
+#include "fsw/coroutine.h"
 #include "gtest/gtest.h"
 
 using fsw::Process;
+using fsw::Coroutine;
 
 static void child_handler()
 {
-    std::cout << "handler: " << getpid() << std::endl;
+    std::cout << Coroutine::get_current()->get_cid() << std::endl;
 }
 
 TEST(process, construct)
 {
     Process process(std::bind(child_handler));
+}
+
+TEST(process, construct_enable_coroutine)
+{
+    bool ret;
+
+    Process process(std::bind(child_handler), true);
+    ret = process.start();
+    ASSERT_EQ(ret, true);
+    ASSERT_EQ(process.pid, getpid());
+    process.wait(); // Do not delete this line, or TEST(process, wait) will test fail
 }
 
 TEST(process, start)
