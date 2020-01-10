c64b127a9dbce7a3f4572e25d8c92653c4da9df4
codinghuang <2812240764@qq.com>
2020-01-10 10:30:03 +0000

    refactor: refactor event
    add  class -- fsw::Event

--- include/event.h
+++ include/event.h
@@ -5,42 +5,46 @@
 
 #define FSW_EPOLL_CAP 16;
 
-namespace fsw { namespace event {
-
-typedef struct
+namespace fsw {
+struct Poll
 {
     int epollfd;
     int ncap;
     int event_num;
     struct epoll_event *events;
-} fswPoll_t;
+};
+
+class Event
+{
+public:
+    enum type
+    {
+        FSW_EVENT_NULL   = 0,
+        FSW_EVENT_DEAULT = 1u << 8,
+        FSW_EVENT_READ   = 1u << 9,
+        FSW_EVENT_WRITE  = 1u << 10,
+        FSW_EVENT_RDWR   = FSW_EVENT_READ | FSW_EVENT_WRITE,
+        FSW_EVENT_ERROR  = 1u << 11,
+    };
+
+    int running;
+    struct Poll *poll;
+
+    Event();
+    ~Event();
+    int wait();
+
+private:
+    int init_Poll();
+    int free_Poll();
+};
 
 typedef struct
 {
-    int running;
-    fswPoll_t *poll;
-} fswGlobal_t;
+    Event *event;
+} Global_t;
 
-extern fswGlobal_t FswG;
-
-enum fswEvent_type
-{
-    FSW_EVENT_NULL   = 0,
-    FSW_EVENT_DEAULT = 1u << 8,
-    FSW_EVENT_READ   = 1u << 9,
-    FSW_EVENT_WRITE  = 1u << 10,
-    FSW_EVENT_RDWR   = FSW_EVENT_READ | FSW_EVENT_WRITE,
-    FSW_EVENT_ERROR  = 1u << 11,
-};
-
-int init_fswPoll();
-inline int free_fswPoll();
-
-int fsw_event_init();
-int fsw_event_wait();
-int fsw_event_free();
-
-}
+extern Global_t FswG;
 }
 
 #endif /* EVENT_H_ */
