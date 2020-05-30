2adbc98df7686aa17e8af5ae3168381abe462a8f
codinghuang <2812240764@qq.com>
2020-05-30 16:36:33 +0000

    example: update http2::client example

--- examples/coroutine/http2/client/client.cc
+++ examples/coroutine/http2/client/client.cc
@@ -10,8 +10,15 @@ int main(int argc, char const *argv[])
 {
     run([]()
     {
+        bool ret;
         Client h2c;
         Request req;
+
+        ret = h2c.connect("127.0.0.1", 80);
+        if (!ret)
+        {
+            fswError("%s", h2c.sock->get_err_msg());
+        }
         h2c.send_request(&req);
         return 0;
     });
